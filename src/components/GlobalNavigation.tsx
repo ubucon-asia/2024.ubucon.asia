2d3b96d801a0c1bbfa2c62a78a780365db154f68
Youngbin Han <youngbin.han@cloudmt.co.kr>
2024-01-11 16:25:42 +0900

    Initial Main page

---
+++ src/components/GlobalNavigation.tsx
@@ -0,0 +1,27 @@
+import {
+	Navigation, Theme
+} from "@canonical/react-components";
+import type {
+	NavItem
+} from "@canonical/react-components";
+
+type GlobalNavigationProps = {
+	logoTitle: string
+	navItems: NavItem[]
+	navItemsRight: NavItem[]
+}
+export default function GlobalNavigation(props: GlobalNavigationProps) {
+
+	return (
+		<Navigation
+			items={props.navItems}
+			itemsRight={props.navItemsRight}
+			logo={{
+				src: 'https://assets.ubuntu.com/v1/82818827-CoF_white.svg',
+				title: props.logoTitle,
+				url: '#'
+			}}
+			theme={Theme.DARK}
+		/>
+	)
+}
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,35 @@
 export const config = {
-    siteTitle: "UbuCon Asia 2024"
+    siteTitle: "UbuCon Asia 2024",
+    navigation: [
+        {
+            label: 'About',
+            url: '#'
+        },
+        {
+            label: 'Venue & Travel',
+            url: '#'
+        },
+        {
+            label: 'Programs',
+            url: '#'
+        },
+        {
+            label: 'Sponsors/Patrons',
+            url: '#'
+        },
+        {
+            label: 'News',
+            url: '#'
+        },
+        {
+            label: 'Docs',
+            url: '#'
+        }
+    ],
+    navigationRight: [
+        {
+            label: 'Register',
+            url: '#'
+        }
+    ]
 }
