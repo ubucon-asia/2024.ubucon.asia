c5a80786d4b68b5e16d231a0a644042ab45bf270
Youngbin Han <ybhan@ubuntu.com>
2024-01-14 02:25:33 +0900

    Add footer

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,18 @@
+import UbuntuIndiaLogo from "./UbuntuIndia.svg"
+
 export const config = {
     siteTitle: "UbuCon Asia 2024",
-    blog:{
-        rssFeedUrl:"https://blog.ubucon.asia/tags/ubucon/index.xml",
+    blog: {
+        rssFeedUrl: "https://blog.ubucon.asia/tags/ubucon/index.xml",
         viewMoreUrl: "https://blog.ubucon.asia/"
+    },
+    footer: {
+        copyright: "© 2024-Present UbuCon Asia Committee. Ubuntu and Canonical are registered trademarks of Canonical Ltd. Unless otherwise noted, content licensed under CC BY 4.0 source code under MIT.",
+        contactUs: "mailto:contact@ubucon.asia",
+        srcRepoUrl: "https://github.com/ubucon-asia/2024.ubucon.asia",
+        organizers: [
+            { name: "Ubuntu India", logoImage: UbuntuIndiaLogo.src, link: "https://www.ubucon.asia" }
+        ]
     },
     navigation: [
         {
