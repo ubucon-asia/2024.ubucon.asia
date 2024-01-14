4d86d80c107c9d63fab1bda13ad3557f75fe8ab3
Youngbin Han <ybhan@ubuntu.com>
2024-01-15 02:40:26 +0900

    Add The Organizing Committee info on about page

---
+++ src/data/committeeMembers.ts
@@ -0,0 +1,65 @@
+import SyazwanProfile from "@assets/profile/syazwan.png";
+import BhavaniProfile from "@assets/profile/bhavani.jpeg";
+import uca24Logo from "@assets/logo.svg";
+export const committeeMembers = [
+    {
+        name: "Youngbin Han",
+        team: "Global team & Ubuntu Korea Community, Ubuntu LoCo Council",
+        role: "Allrounder - Usually sponsorship, web and content review",
+        profileImageUrl: "https://avatars.githubusercontent.com/u/1916739?v=4",
+    },
+    {
+        name: "Khairul Aizat Bin Kamarudzzaman",
+        team: "Global team & Ubuntu Malaysia, Ubuntu Membership Board",
+        role: "Sponsorship, Web, Content",
+        profileImageUrl: "https://avatars.githubusercontent.com/u/405473?v=4",
+    },
+    {
+        name: "Muhd Syazwan",
+        team: "Global team & Ubuntu Malaysia",
+        role: "Web, Content",
+        profileImageUrl: SyazwanProfile.src,
+    },
+    {
+        name: "RJ Hsiao",
+        team: "Global team & Ubuntu Taiwan",
+        role: "Marketing",
+        profileImageUrl: "https://avatars.githubusercontent.com/u/236116?v=4",
+    },
+    {
+        name: "Aryan Kaushik",
+        team: "Local team & Ubuntu India",
+        role: "Local team Chief, Sponsorship, Venue, Content",
+        profileImageUrl: "https://avatars.githubusercontent.com/u/34372791?v=4",
+    },
+    {
+        name: "Bhavanishankar Ravindra",
+        team: "Local team & Ubuntu India, Ubuntu LoCo Council",
+        role: "Local team Chief, Sponsorship, Venue, Content",
+        profileImageUrl: BhavaniProfile.src,
+    },
+    {
+        name: "Asmit Malakannawar",
+        team: "Local team & Ubuntu India, GNOME Foundation member",
+        role: "Design, Travel support",
+        profileImageUrl: "https://avatars.githubusercontent.com/u/81866614?v=4",
+    },
+    {
+        name: "Devanshu Saxena",
+        team: "Local team & Ubuntu India",
+        role: "?", //FIXME: Add role
+        profileImageUrl: "https://avatars.githubusercontent.com/u/86067292?v=4",
+    },
+    {
+        name: "Deepesha Burse",
+        team: "Local team & Ubuntu India, GNOME Foundation member",
+        role: "?", //FIXME: Add role
+        profileImageUrl: "https://avatars.githubusercontent.com/u/87636253?v=4",
+    },
+    {
+        name: "Srijan Srivastava",
+        team: "Local team & Ubuntu India, PythonDelhi",
+        role: "Sponsorship",
+        profileImageUrl: uca24Logo.src,
+    }
+]
