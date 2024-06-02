bf140afc13dc8205ec869ca959b48764db8076eb
Youngbin Han <ybhan@ubuntu.com>
2024-06-02 15:08:35 +0900

    Update featured speakers

---
+++ src/data/featured.ts
@@ -0,0 +1,18 @@
+
+import speaker489 from "@assets/speaker_picture/489.png";
+import speaker478 from "@assets/speaker_picture/478.jpeg";
+import speaker504 from "@assets/speaker_picture/504.jpeg";
+
+export const featuredSessionIds = [
+   371, 390
+];
+
+interface ImageMapType {
+    [key: string]: ImageMetadata
+  };
+
+export const speakerPersonIdPictureMap: ImageMapType = {
+    k489: speaker489,
+    k478: speaker478,
+    k504: speaker504
+}
