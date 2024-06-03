482ac2e26616ba87a22bcf9955e1c18b2b0909b7
Youngbin Han <sukso96100@gmail.com>
2024-06-03 13:15:03 +0900

    Add Till as featured speaker

--- src/data/featured.ts
+++ src/data/featured.ts
@@ -2,9 +2,10 @@
 import speaker489 from "@assets/speaker_picture/489.png";
 import speaker478 from "@assets/speaker_picture/478.jpeg";
 import speaker504 from "@assets/speaker_picture/504.jpeg";
+import speaker500 from "@assets/speaker_picture/500.png";
 
 export const featuredSessionIds = [
-   371, 390
+   371, 390, 397
 ];
 export const sessionListApiUrl = "https://events.canonical.com/export/event/47.json?detail=contributions&pretty=yes:";
 export const sessionListUrl = "https://events.canonical.com/event/47/contributions/";
@@ -15,5 +16,6 @@ interface ImageMapType {
 export const speakerPersonIdPictureMap: ImageMapType = {
     k489: speaker489,
     k478: speaker478,
-    k504: speaker504
+    k504: speaker504,
+    k500: speaker500
 }
